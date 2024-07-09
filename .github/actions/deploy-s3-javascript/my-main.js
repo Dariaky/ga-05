bb1a9b0753bd7702ef5c10a73b9ac3be701cc590
dkyrychenko <dkyrychenko@binariks.com>
2024-07-09 16:58:54 +0200

    add deploy to s3 bucket, add inputs and outputs

--- .github/actions/deploy-s3-javascript/my-main.js
+++ .github/actions/deploy-s3-javascript/my-main.js
@@ -2,7 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run() {
-  core.notice('Hello World!');
+  // 1) Get the inputs
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+  
+  // 2) Upload the files. This command will first look for auth keys in the env.
+  exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
+  
+   const websiteUrl = `http://${{ bucket }}.s3-website-${{ bucketRegion }}.amazonaws.com`;
+   core.setOutput('website-url', websiteUrl);
+  
+  // core.notice('Hello World!');
 }
 
 run();
